4b22bd9f63329d77e9a5dca2c1a8512eff291710
Han <elinyuhanwu@gmail.com>
2020-08-12 17:49:47 +0800

    configure http header

--- server.js
+++ server.js
@@ -1,19 +1,28 @@
 const express = require('express')
 const cors = require('cors')
 const helmet = require('helmet')
-var winston = require('winston');
+var winston = require('winston')
+const morgan = require('morgan')
 
 const bodyParser = require('body-parser');
 const app = express()
 app.use(cors())
 app.use(helmet())
+// app.use(morgan('combined'))
 app.use(bodyParser.json())
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => {
+  res.cookie('session', '1', { httpOnly: true });
+  res.cookie('session', '1', { secure: true });
+  res.set({
+    'Content-Security-Policy': 'script-src "self" "https://apis.google.com"'
+  })
+  res.send('Hello World!')
+})
 
 app.post('/secret', (req, res) => {
   const { userInput } = req.body;
-  console.log(userInput);
+  winston.log(userInput);
   if (userInput) {
     winston.log('info', 'user input: ' + userInput);
     res.status(200).json('success');
